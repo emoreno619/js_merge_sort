1ff7d087ccce9c2e5cfb0ba3b631f352fc13c547
Eduardo Moreno <eduardo@Eduardos-MacBook-Pro.local>
2015-05-13 14:38:32 -0700

    initial commit

--- mergeSort.js
+++ mergeSort.js
@@ -3,6 +3,29 @@
 // returns one array which contains the sorted numbers
 // from left and right.
 exports.merge = function(left, right) {
+	var leftI = 0;
+	var rightI = 0;
+	var res = [];
+	while(leftI < left.length || rightI < right.length){
+		if(leftI === left.length){
+			res.push(right[rightI]);
+			rightI++;
+		}
+		 else if(rightI === right.length){
+			res.push(left[leftI]);
+			leftI++;
+		} else {
+			if(left[leftI] <= right[rightI]){
+				res.push(left[leftI]);
+				leftI++;
+			} else if(right[rightI] < left[leftI]){
+				res.push(right[rightI]);
+				rightI++;
+			}
+		}
+		
+	}
+	return res;
 }
 
 // Merge sort uses the merge function in order to
@@ -10,5 +33,17 @@ exports.merge = function(left, right) {
 // order, merge sort should return an array that is
 // sorted.
 exports.mergeSort = function(arr) {
+	//base case
+	if(arr.length < 2){
+		return arr;
+	} else {
+		var mid = Math.floor(arr.length/2);
+		var left = arr.slice(0,mid);
+		var right = arr.slice(mid, arr.length);
+		var sortL = exports.mergeSort(left);
+		var sortR = exports.mergeSort(right);
+		return exports.merge(sortL, sortR);
+	}
+
 }
 
--- spec/mergeSortSpec.js
+++ spec/mergeSortSpec.js
@@ -7,9 +7,45 @@ describe('Merge', function() {
 
 });
 
-describe('Merge Sort', function() {
-  it('should sort two arrays of size 1', function() {
+describe('Merge', function() {
+  it('should merge two arrays of size 1', function() {
      expect(mergeSort.merge([2],[1])).toEqual([1,2]);
   });
 });
 
+describe('Merge', function() {
+  it('should merge two sorted arrays of each size 2', function() {
+     expect(mergeSort.merge([1,2],[1,3])).toEqual([1,1,2,3]);
+  });
+});
+
+describe('Merge', function() {
+  it('should sort two sorted arrays of different sizes', function() {
+     expect(mergeSort.merge([2],[1,3])).toEqual([1,2,3]);
+  });
+});
+
+describe('Merge', function() {
+  it('should sort two sorted arrays of different sizes', function() {
+     expect(mergeSort.merge([2],[1,3,5])).toEqual([1,2,3,5]);
+  });
+});
+
+describe('Merge', function() {
+  it('should sort two sorted arrays of different sizes', function() {
+     expect(mergeSort.merge([2,6,10],[1,3])).toEqual([1,2,3,6,10]);
+  });
+});
+
+describe('Merge', function() {
+  it('should sort two sorted arrays of different sizes', function() {
+     expect(mergeSort.merge([2,6,10],[3])).toEqual([2,3,6,10]);
+  });
+});
+
+describe('Merge Sort', function() {
+  it('should sort an unsorted array', function() {
+     expect(mergeSort.mergeSort([12,6,10,3])).toEqual([3,6,10,12]);
+     expect(mergeSort.mergeSort([12,6,10,3,1])).toEqual([1,3,6,10,12]);
+  });
+});
